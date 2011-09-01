d79563e71dd829a1b54fe2530fe6d60ac6f7c4b1
Bryan Taylor <bryan@rubyisbeautiful.com>
2011-09-01 08:52:43 -0700

    fix the form url for new @page set the I18n locale to the full language-country

--- app/controllers/comatose/application_controller.rb
+++ app/controllers/comatose/application_controller.rb
@@ -13,10 +13,10 @@ module Comatose
       locale_language, locale_country = params[:locale].to_s.split('-')
       locale_language ||= 'en'
       locale_country  ||= 'US'
-      locale_language.downcase.to_sym
+      locale_language.downcase!
       locale_country.upcase!
-      I18n.locale = locale_language
-      logger.debug "Locale set to #{locale_language} from param #{params[:locale]}"
+      I18n.locale = "#{locale_language}-#{locale_country}"
+      logger.debug "Locale set to #{I18n.locale} from param #{params[:locale]}"
     end
 
 
